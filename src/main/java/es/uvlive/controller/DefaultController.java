1e35e5dd5b8f6c808e2165e427fa0c17ea654f42
Alex Traver <alejandro.traver.fos@everis.com>
2016-01-06 17:37:32 +0100

    Welcome Message

--- src/main/java/es/uvlive/controller/DefaultController.java
+++ src/main/java/es/uvlive/controller/DefaultController.java
@@ -19,7 +19,7 @@ public class DefaultController {
     
    @RequestMapping(value = "/", method = RequestMethod.GET)
    public String index(ModelMap map) {
-       map.put("msg", "Hello Spring 4 Web MVC!");
+       map.put("msg", "Enhorabuena, el Servidor se ha lanzado correctamente!");
        return "index";
    }
     
