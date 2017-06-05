80259a2e929a740897d0cbc7f8e86fb072b49270
Yvette Hernández <yvette.hernandez.alonso@everis.com>
2017-06-05 09:54:21 +0200

    Update - Temporally modifying database structure (Message table)

--- VisualParadigm/database/UVLive.sql
+++ VisualParadigm/database/UVLive.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Message (idMessage int(11) NOT NULL AUTO_INCREMENT, text varchar(1023), timestamp int(13), StudentidStudent int(11) NOT NULL, TeacheridTeacher int(11) NOT NULL, ConversationidConversation int(11) NOT NULL, RolUVidUser int(11) NOT NULL, PRIMARY KEY (idMessage));
+CREATE TABLE Message (idMessage int(11) NOT NULL AUTO_INCREMENT, text varchar(1023), timestamp int(13), ConversationidConversation int(11) NOT NULL, RolUVidUser int(11) NOT NULL, PRIMARY KEY (idMessage));
 CREATE TABLE Businessman (idUser int(11) NOT NULL AUTO_INCREMENT, dni varchar(9), PRIMARY KEY (idUser));
 CREATE TABLE Broadcast (idBroadcast int(11) NOT NULL AUTO_INCREMENT, text int(255), ComercianteidComerciante int(11), PRIMARY KEY (idBroadcast));
 CREATE TABLE Teacher (idUser int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY (idUser));
 CREATE TABLE Student (idUser int(11) NOT NULL AUTO_INCREMENT, blocked int(1), PRIMARY KEY (idUser));
 CREATE TABLE Conversation (idTutorial int(11) NOT NULL AUTO_INCREMENT, name varchar(52), PRIMARY KEY (idTutorial));
-CREATE TABLE `User` (idUser int(11) NOT NULL AUTO_INCREMENT, userName varchar(8), password varchar(18), firstName varchar(63), lastName varchar(63), PRIMARY KEY (idUser));
+CREATE TABLE User (idUser int(11) NOT NULL AUTO_INCREMENT, userName varchar(8), password varchar(18), firstName varchar(63), lastName varchar(63), PRIMARY KEY (idUser));
 CREATE TABLE RolUV (idUser int(11) NOT NULL AUTO_INCREMENT, pushToken varchar(255), PRIMARY KEY (idUser));
 CREATE TABLE Admin (idUser int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY (idUser));
 CREATE TABLE Conversation_RolUV (ConversationidTutorial int(11) NOT NULL, RolUVidUser int(11) NOT NULL, PRIMARY KEY (ConversationidTutorial, RolUVidUser));
